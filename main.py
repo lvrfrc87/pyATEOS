87efdee6ad2be8255a37ad5aea3f60e4c5438771
federico.olivieri <federico.olivieri@ticketmaster.co.uk>
2020-02-03 16:50:38 +0100

    DRAFT: add plugin iface

--- main.py
+++ main.py
@@ -6,26 +6,31 @@ from pyeapi import load_config
 from pyeapi import connect_to
 from pyeapi import connect_to
 from plugins.ntp import Ntp
+from plugins.snmp import Snmp
+from plugins.interfaces import Interfaces
 from colorama import Fore, Back, Style, init
 
 load_config('eos_inventory.ini')
 node = connect_to('localhost')
 
-filename = sys.argv[1]
 ntp = Ntp(node).associations
-file = open('ntp_{}.json'.format(filename),'w')
-for i in ntp:
+snmp = Snmp(node).host
+iface = Interfaces(node).show
+
+final_list = ntp + snmp + iface
+file = open('{}.txt'.format(sys.argv[1]),'w')
+for i in final_list:
     file.write(str(i))
 file.close()
 
-text1 = open("ntp_yesterday.json").readlines()
-text2 = open("ntp_today.json").readlines()
+text1 = open("before.txt").readlines()
+text2 = open("after.txt").readlines()
 
 diff = difflib.unified_diff(
     text1,
     text2,
-    fromfile='ntp_yesterday.json',
-    tofile='ntp_today.json',
+    fromfile='before.txt',
+    tofile='after.txt',
     lineterm='',
     n=0)
 
